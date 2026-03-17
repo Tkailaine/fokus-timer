3d3aafab4f423ae837839e691c84f61876f4cfb8
thaissa Kailaine <thaissakailaine.ti@gmail.com>
2026-03-17 08:44:06 -0300

    feat: selecionando tarefas e adicionando descrições na tela

--- script-crud.js
+++ script-crud.js
@@ -1,15 +1,23 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const adicionarTarefa = document.querySelector('.app__form-add-task')
 const inputTarefa = document.querySelector('.app__form-textarea')
-const btnSalvarTarefa = document.querySelector('.app__form-footer__button  ')
+const btnSalvarTarefa = document.querySelector('.app__form-footer__button--confirm')
+const listaTarefas = document.querySelector('.app__section-task-list')
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
+let tarefaSelecionada = null
+
 
 // Lista de tarefas
-const tarefas = []
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
 //Mostrar interface de tarefa
 btnAdicionarTarefa.addEventListener('click', () => {
    adicionarTarefa.classList.toggle('hidden')
 })
+
+function atualizarTarefa(){
+     localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
 
 adicionarTarefa.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -17,5 +25,76 @@ adicionarTarefa.addEventListener('submit', (event) => {
         descricao : inputTarefa.value
     }
     tarefas.push(tarefa)
-    localStorage.setItem('Tarefas', JSON.stringify(tarefas))
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    listaTarefas.append(elementoTarefa)
+    atualizarTarefa()
+    //Limpando textarea
+    inputTarefa.value = '';
+    //ocultar formulário
+    adicionarTarefa.classList.add('hidden')
 })
+
+function criarElementoTarefa(tarefa){
+    const li = document.createElement('li')
+    li.classList.add('app__section-task-list-item')
+
+    const svg = document.createElement('svg')
+    svg.innerHTML = `
+    <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+    `
+
+    const paragrafo = document.createElement('p')
+    paragrafo.textContent = tarefa.descricao
+    paragrafo.classList.add('app__section-task-list-item-description')
+
+    const botao = document.createElement('button')
+    botao.classList.add('app_button-edit')
+    const imagemBotao = document.createElement('img')
+    imagemBotao.setAttribute('src', 'imagens/edit.png')
+    
+    botao.onclick = () => {
+    const novaDescricao = prompt('Qual é o novo nome da tarefa?')
+    if(novaDescricao){
+         paragrafo.textContent = novaDescricao
+         tarefa.descricao = novaDescricao
+         atualizarTarefa()}
+    }
+   
+
+    botao.append(imagemBotao)
+    li.append(svg)
+    li.append(paragrafo)
+    li.append(botao)
+
+    li.onclick = () => { 
+        //Deseleciona todas as tarefas antes de selecionar outra
+        document.querySelectorAll('.app__section-task-list-item-active')
+        .forEach(tarefa => {
+            tarefa.classList.remove('app__section-task-list-item-active')
+        })
+        li.classList.add('app__section-task-list-item-active')
+        //remove descrição ao deselecionar tarefa
+        if(tarefaSelecionada == tarefa){
+            paragrafoDescricaoTarefa.textContent = ''
+            tarefaSelecionada = null
+            return
+
+        }
+        tarefaSelecionada = tarefa
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao
+       
+    }
+
+    return li
+
+}
+
+tarefas.forEach(tarefa => {
+   const elementoTarefa = criarElementoTarefa(tarefa)
+    listaTarefas.append(elementoTarefa)
+
+} )
+
